9725c510ad3093cbb257a1db281abf840f185044
benderTheCrime <joseph.groseclose@spongecell.com>
2015-10-01 22:32:34 -0400

    Removed dependency tasks from babel/esdoc

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -67,10 +67,10 @@ gulp.task('cobertura', [ 'mocha:src' ], function(cb) {
     cobertura('coverage/cobertura-coverage.xml', 'svg', cb);
 });
 
-gulp.task('esdoc', [ 'cobertura' ], function() {
+gulp.task('esdoc', function() {
     return gulp.src(SRC_DIR).pipe(esdoc({ destination: DOC_SRC }));
 });
-gulp.task('babel', [ 'esdoc' ], function() {
+gulp.task('babel', function() {
     return gulp.src(SRC).pipe(babel({
         comments: false
     })).pipe(gulp.dest(TRANSPILED_SRC_DIR));
